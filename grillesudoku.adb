a949b2f62d45113fd8809bc68131d5f8555a0256
Mathieu Barrières <mathieu.barrieres@etu.iut-tlse3.fr>
2021-01-18 11:24:50 +0100

    grille sudoku faite

--- grillesudoku.adb
+++ grillesudoku.adb
@@ -9,6 +9,95 @@ package body grilleSudoku is
    function construireGrille return Type_Grille is
      g : Type_Grille;
    begin
+      g(1, 1) := 0;
+      g(1, 2) := 0;
+      g(1, 3) := 0;
+      g(1, 4) := 0;
+      g(1, 5) := 0;
+      g(1, 6) := 0;
+      g(1, 7) := 0;
+      g(1, 8) := 0;
+      g(1, 9) := 0;
+
+      g(2, 1) := 0;
+      g(2, 2) := 0;
+      g(2, 3) := 0;
+      g(2, 4) := 0;
+      g(2, 5) := 0;
+      g(2, 6) := 0;
+      g(2, 7) := 0;
+      g(2, 8) := 0;
+      g(2, 9) := 0;
+
+      g(3, 1) := 0;
+      g(3, 2) := 0;
+      g(3, 3) := 0;
+      g(3, 4) := 0;
+      g(3, 5) := 0;
+      g(3, 6) := 0;
+      g(3, 7) := 0;
+      g(3, 8) := 0;
+      g(3, 9) := 0;
+
+      g(4, 1) := 0;
+      g(4, 2) := 0;
+      g(4, 3) := 0;
+      g(4, 4) := 0;
+      g(4, 5) := 0;
+      g(4, 6) := 0;
+      g(4, 7) := 0;
+      g(4, 8) := 0;
+      g(4, 9) := 0;
+
+      g(5, 1) := 0;
+      g(5, 2) := 0;
+      g(5, 3) := 0;
+      g(5, 4) := 0;
+      g(5, 5) := 0;
+      g(5, 6) := 0;
+      g(5, 7) := 0;
+      g(5, 8) := 0;
+      g(5, 9) := 0;
+
+      g(6, 1) := 0;
+      g(6, 2) := 0;
+      g(6, 3) := 0;
+      g(6, 4) := 0;
+      g(6, 5) := 0;
+      g(6, 6) := 0;
+      g(6, 7) := 0;
+      g(6, 8) := 0;
+      g(6, 9) := 0;
+
+      g(7, 1) := 0;
+      g(7, 2) := 0;
+      g(7, 3) := 0;
+      g(7, 4) := 0;
+      g(7, 5) := 0;
+      g(7, 6) := 0;
+      g(7, 7) := 0;
+      g(7, 8) := 0;
+      g(7, 9) := 0;
+
+      g(8, 1) := 0;
+      g(8, 2) := 0;
+      g(8, 3) := 0;
+      g(8, 4) := 0;
+      g(8, 5) := 0;
+      g(8, 6) := 0;
+      g(8, 7) := 0;
+      g(8, 8) := 0;
+      g(8, 9) := 0;
+
+      g(9, 1) := 0;
+      g(9, 2) := 0;
+      g(9, 3) := 0;
+      g(9, 4) := 0;
+      g(9, 5) := 0;
+      g(9, 6) := 0;
+      g(9, 7) := 0;
+      g(9, 8) := 0;
+      g(9, 9) := 0;
      return g;
    end construireGrille;
 
@@ -137,7 +226,7 @@ package body grilleSudoku is
 
    function obtenirChiffresDUneLigne
     (g : in Type_Grille; numLigne : in Integer) return Type_Ensemble
-  is
+   is
    begin
      pragma Compile_Time_Warning
        (Standard.True, "obtenirChiffresDUneLigne unimplemented");
