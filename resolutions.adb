abc9e9e5726c28980bd3532fa9f1f0f3079e7897
unknown <enzo.slamnia@etu.iut-tlse3.fr>
2021-01-18 10:25:10 +0000

    2e fonction de resolution

--- resolutions.adb
+++ resolutions.adb
@@ -32,12 +32,26 @@ package body resolutions is
    function obtenirSolutionsPossibles
      (g : in Type_Grille; c : in Type_Coordonnee) return Type_Ensemble
    is
+      valeurs:Type_Ensemble;
+      colonne:Type_Ensemble;
+      ligne:Type_Ensemble;
+      carre:Type_Ensemble;
+      i:Integer;
    begin
-      pragma Compile_Time_Warning
-        (Standard.True, "obtenirSolutionsPossibles unimplemented");
-      return
-        raise Program_Error
-          with "Unimplemented function obtenirSolutionsPossibles";
+      if not caseVide(g,c) then
+         raise CASE_NON_VIDE;
+      end if;
+
+      ligne:=obtenirChiffresDUneLigne(g,obtenirLigne(c));
+      colonne:=obtenirChiffresDUneColonne(g,obtenirColonne(c));
+      carre:=obtenirChiffresDUnCarre(g,obtenirCarre(c));
+
+      while i < 9 loop
+         if not appartientChiffre(ligne,i) and not appartientChiffre(colonne,i)
+           and not appartientChiffre(carre,i) then
+            valeurs(i):=true;
+         i:=i+1;
+      end loop;
    end obtenirSolutionsPossibles;
 
    ------------------------------------------
